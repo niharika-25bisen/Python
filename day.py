7fadc98026db008d0df13d86a25a4596250b7647
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-11 20:42:14 +0530

    Create day.py

---
+++ day.py
@@ -0,0 +1,6 @@
+#WAP to accept working days
+
+day=input ("Enter your day:")
+if day=="Saturday"or day=="Sunday" or day=="saturday" or day=="sunday":print("Its weekend")
+else: 
+    print ("Working days")
