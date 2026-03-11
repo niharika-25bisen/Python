0f772d6ecab22e88d1d7914b09b3b1fc993be3a2
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-11 21:23:03 +0530

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,18 @@
+print('Subjects marks:')
+
+phy = 50
+chem = 60
+math = 70
+
+
+print("physics ={} chemistry ={} math ={}".format(phy, chem, math))
+
+
+print("physics ={0} chemistry ={1} math ={2}".format(phy, chem, math))
+
+print("physics ={x} chemistry ={y} math ={z}".format(x=phy, y=chem, z=math))
+
+
+total = phy + chem + math
+print("total marks =", total)  
+print(f"total marks = {total}") 
