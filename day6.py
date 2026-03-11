ee7bc73c1b571402619512a274d751f6dfee1387
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-11 21:20:10 +0530

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,35 @@
+name = "prashantjha"
+#indexing = 012345678910
+print(name[0]) #p
+print(name[1])
+print(name[-1])
+
+print(name[0:5])
+print(name[1:])
+print(name[:5])
+print(name[:])
+print(name[1:8:2])
+print(name[::-1])
+
+
+
+s="helpcode is a best platform from practising programming"
+print(s.find("helpcode"))
+print(s.find("python"))
+print(s.find("programming"))
+
+
+
+s="prashant","ashish","sandip"
+m='&'.join(s)
+print(m)
+
+
+s="Python is a High level programming Language"
+print(s.lower())
+print(s.upper())
+print(s.swapcase())
+print(s.title())
+print(s.capitalize())
+
+
