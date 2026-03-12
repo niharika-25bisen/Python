7b4f316fa78b656c4d82f70610aed7c45d479516
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-12 12:49:34 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -1,13 +1,2 @@
-#WAP to if percentage is greater then 90 so assign grade A if percentage greater then 80 assign grade B and if percentage is greater then 60 so assign grade C and if percentage is below 60 so print fail
 
-per =int(input("Enter your percentage:") )
-
-if per>=90:
-    print("Grade A")
-elif per>=80 and per<90:
-    print("Grade B")
-elif per>=60 and per<80:
-    print("Grade C")
-else:
-    print("FAIL")
 
