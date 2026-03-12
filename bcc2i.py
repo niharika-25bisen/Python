db6021838406ed81801cc0cd6f27dd9d2ecbc36b
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-12 14:16:28 +0530

    Update bcc2i.py

--- bcc2i.py
+++ bcc2i.py
@@ -1,5 +1,7 @@
-rollno = [3,5,7,1,11,4,5,2]
-for x in rollno :
-    if x==2 or x==4 or x==6 or x==8 or x==10:
-        print("even no is found",x)
+name ="prashant"
+i=0
+for x in name :
+    if x=='n':
+        print("The chaaracter present at index no.",i," value=:",x)
         break
+    i=i+1
