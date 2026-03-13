1ad93bffb6c41e7296460c46147937b6f330c7f0
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-13 14:36:29 +0530

    Update t3g.py

--- t3g.py
+++ t3g.py
@@ -1,7 +1,6 @@
 #nested loop
 
-for i in range(1,4):    #inner loop
-    for j in range(1,4):      #outer loop
+for i in range(1,4):    #outer loop
+    for j in range(1,4):      #inner loop
         print(i, end=" ")
     print()
-    
