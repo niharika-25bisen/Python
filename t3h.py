ebafcfcb9014f13a9c9162870d541f72c8e01f5c
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-13 14:45:11 +0530

    Create t3h.py

---
+++ t3h.py
@@ -0,0 +1,20 @@
+#n=int(input("Enter the number of rows:"))
+#for i in range(1,n+1):
+    #for j in range(1,n+1):
+     #   print(chr(64+i),end=" ")
+    #print()
+
+
+
+#n=int(input("Enter the number of rows:"))
+#for i in range(1,n+1):
+ #   for j in range(1,n+1):
+  #      print(n+1-i,end=" ")
+   # print()
+
+
+n=int(input("Enter number of rows:"))
+for i in range(1,n+1):
+    for j in range(1,1+i):
+        print(i,end=" ")
+    print()
