7998468e106eb6b7973b563faa4059495032a367
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-13 14:16:36 +0530

    Update t3d.py

--- t3d.py
+++ t3d.py
@@ -7,3 +7,16 @@ mydict = {
     104:"bisen",
 }
 print(mydict)
+
+#for x in mydict:                 #printing keys
+   # print(x)
+
+#for x in mydict.values():        #printing values
+       # print(x)
+
+#for x,y in mydict.items():         #printing the keys and values both
+    #print(x,y)
+
+#mydict["mobile_no"]= 4646463738      #if i have to ad new key and value pair in my dictionary
+#print(mydict)
+mydict.pop(101)
