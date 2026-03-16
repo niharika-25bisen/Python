ecbae80c81e6e0c90731613749edd66d60c286af
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-16 13:21:30 +0530

    Update m2.py

--- m2.py
+++ m2.py
@@ -1,5 +1,8 @@
 def msg():
     val1 = int(input("Enter first value:"))
     val2 = int(input("Enter first value:"))
-    print(val1+val2)
-msg()
+    #print(val1+val2) method 1
+    return val1+val2       
+#print(msg())          method 2
+res = msg()
+print("Result=",res)
