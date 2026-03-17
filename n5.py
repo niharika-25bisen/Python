2d020708ff30f50a5ac6174475965d21fbaef20a
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-17 14:08:54 +0530

    Update n5.py

--- n5.py
+++ n5.py
@@ -1,12 +1,12 @@
 myset ={1,2,"sanjay",5.66,"rahul","ayush","rahul","ramesh","ankit",'rishikesh'}
 print(myset)
-#print(myset[0])
+#print(myset[0])        #error
 
-#myset.add(60)
+#myset.add(60)  
 #print(myset)
 
 #myset.discard(3)
 #print(myset)
 
 myset.remove(3)
-print(myset)
+print(myset)               #error
