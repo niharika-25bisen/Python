26c80da044888616f05c6883c6a04fe19479605d
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-17 14:01:01 +0530

    Create n5.py

---
+++ n5.py
@@ -0,0 +1,12 @@
+myset ={1,2,"sanjay",5.66,"rahul","ayush","rahul","ramesh","ankit",'rishikesh'}
+print(myset)
+#print(myset[0])
+
+#myset.add(60)
+#print(myset)
+
+#myset.discard(3)
+#print(myset)
+
+myset.remove(3)
+print(myset)
