8f3a2a759f46b556b9515e286ead4b18ba77010e
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-19 13:44:26 +0530

    Create tn5.py

---
+++ tn5.py
@@ -0,0 +1,10 @@
+city=input("Enter your city Name:")
+scity=city.strip()
+if scity=='hyderabad':
+    print("Hello hyderabadi..Adab")
+elif scity =='chennai':
+    print("Hello Madrasi..vannkkam")
+elif scity =="banglore":
+    print("Hello kannadiga..subodaya")
+else:
+    print("your entered city is invalid")
