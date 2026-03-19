7e8b58a5fbc17d9088abe4ff2194d114662085ab
niharika-25bisen <24bisenn@rbunagpur.in>
2026-03-19 14:01:43 +0530

    Update tn6.py

--- tn6.py
+++ tn6.py
@@ -1,3 +1,5 @@
+
+#list compression
 s =[i*i for i in range(1,11)]
 print(s)
 
@@ -6,3 +8,10 @@ print(val)
 
 val2 = [i for i in s if i%2==0]
 print(val2)
+
+#dictionary compression
+squares={x:x*x for x in range(1,6)}
+print(squares)
+
+doubles={x:2*x for x in range(1,6)}
+print(doubles)
